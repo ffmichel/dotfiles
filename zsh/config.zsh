a29646723fb6a137da7402cb8f10e87be8432a4a
ffmichel <social@ffmichel.com>
2015-05-20 00:42:13 +0200

    fix oh my zsh

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -4,8 +4,8 @@ else
   export PS1='%3~$(git_info_for_prompt)%# '
 fi
 
-export LSCOLORS="exfxcxdxbxegedabagacad"
-export CLICOLOR=true
+# export LSCOLORS="exfxcxdxbxegedabagacad"
+# export CLICOLOR=true
 
 fpath=($DOTZSH/functions $fpath)
 
@@ -15,9 +15,9 @@ HISTFILE=~/.zsh_history
 HISTSIZE=10000
 SAVEHIST=10000
 
-setopt NO_BG_NICE # don't nice background tasks
-setopt NO_HUP
-setopt NO_LIST_BEEP
+# setopt NO_BG_NICE # don't nice background tasks
+# setopt NO_HUP
+# setopt NO_LIST_BEEP
 setopt LOCAL_OPTIONS # allow functions to have local options
 setopt LOCAL_TRAPS # allow functions to have local traps
 setopt HIST_VERIFY
