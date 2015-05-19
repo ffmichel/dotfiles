2c82bd60f8f2255f903ecdebf68325a3034ed296
ffmichel <social@ffmichel.com>
2015-05-20 00:42:13 +0200

    fix oh my zsh

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -13,7 +13,7 @@ then
 fi
 
 # Install homebrew packages
-brew install grc coreutils spark python wget tmux ack
+brew install grc coreutils spark python wget tmux ack vim
 
 brew link --overwrite python
 
--- vim/install.sh
+++ vim/install.sh
@@ -6,4 +6,8 @@ echo "installing vim files..."
 if [ ! -d  "$HOME"/.vim/bundle/Vundle.vim ]; then
 	git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
+if [ ! -d  "$HOME"/.powerline-fonts ]; then
+	git clone https://github.com/powerline/fonts.git ~/.powerline-fonts
+fi
+source "$HOME"/.powerline-fonts/install.sh
 
