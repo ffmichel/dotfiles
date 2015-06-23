63e5c27732321e943680c4c29a6f12420e86529b
Fabrice Michel <f7l@3ds.com>
2015-06-23 13:26:32 +0200

    fix zsh

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,4 +1,4 @@
-autoload colors && colors
+autoload -U colors && colors
 # cheers, @ehrenmurdick
 # http://github.com/ehrenmurdick/config/blob/master/zsh/prompt.zsh
 
@@ -43,27 +43,6 @@ need_push () {
     echo " "
   else
     echo " with %{$fg_bold[magenta]%}unpushed%{$reset_color%} "
-  fi
-}
-
-ruby_version() {
-  if (( $+commands[rbenv] ))
-  then
-    echo "$(rbenv version | awk '{print $1}')"
-  fi
-
-  if (( $+commands[rvm-prompt] ))
-  then
-    echo "$(rvm-prompt | awk '{print $1}')"
-  fi
-}
-
-rb_prompt() {
-  if ! [[ -z "$(ruby_version)" ]]
-  then
-    echo "%{$fg_bold[yellow]%}$(ruby_version)%{$reset_color%} "
-  else
-    echo ""
   fi
 }
 
